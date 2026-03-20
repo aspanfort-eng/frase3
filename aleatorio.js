7268582f4bdb23ccb94386d5dedb29d42adeaf6f
aspanfort-eng <aspanfort@gmail.com>
2026-03-20 12:59:24 +0100

    Delete aleatorio.js

--- aleatorio.js
+++
@@ -1,7 +0,0 @@
-function irAFraseAleatoria() {
-    const totalFrases = 11; // Pon aquí cuántas frases tienes
-    const numAzar = Math.floor(Math.random() * totalFrases);
-    // Redirige a la página de frase pasando el número como parámetro
-    window.location.href = `frase.html?id=${numAzar}`;
-}
-
